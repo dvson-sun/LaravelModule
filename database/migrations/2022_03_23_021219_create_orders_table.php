49baf3097087bf0689c96cc5598f266cb06161bb
dvson-sun <100657524+dvson-sun@users.noreply.github.com>
2022-03-23 10:16:00 +0700

    Update database/migrations/2022_03_23_021219_create_orders_table.php
    Co-authored-by: huyenttm-0901 <52224252+huyenttm-0901@users.noreply.github.com>

--- database/migrations/2022_03_23_021219_create_orders_table.php
+++ database/migrations/2022_03_23_021219_create_orders_table.php
@@ -15,7 +15,7 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string("name");
+            $table->string('name');
             $table->bigInteger("user_id")->unique();
             $table->timestamps();
         });
