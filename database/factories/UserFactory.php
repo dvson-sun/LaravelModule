9173b9ca19c58d1ab9411aed56aed42e2837dafb
dvson-sun <dao.vinh.son@sun-asterisk.com>
2022-03-24 09:24:56 +0700

    create db by seeder factory faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -15,10 +16,14 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'username' => $this->faker->userName(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('123456'),
+            'isAdmin' => rand(0,1),
+            'isActive' => 1,
             'remember_token' => Str::random(10),
         ];
     }
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {  
+        // $this->seederAdminAccount();
+        $this->seederUserAccount();
+    }   
+
+    public function seederAdminAccount()
+    {
+        User::create([
+            'first_name' => 'Dao',
+            'last_name' => 'Son',
+            'username' => 'Dao Vinh Son',
+            'isAdmin' => 1,
+            'isActive' => 1,
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('123456'),
+        ]);
+    }
+
+    public function seederUserAccount()
+    {
+        User::factory()->count(10)->create();
+    }
+}
